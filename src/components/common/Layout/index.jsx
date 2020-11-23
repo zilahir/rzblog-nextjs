00fd8e950826202d0a220ba9ef7bab519063f59c
Richard Zilahi <richard.zilahi@salesframe.com>
2020-11-23 02:59:38 +0200

    added tailwind

--- src/components/common/Layout/index.jsx
+++ src/components/common/Layout/index.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
 import { Header } from '@/components/Header';
-import { Grid } from 'react-flexbox-grid';
 
 export const Layout = ({children}) => (
-	<Grid fluid>
+	<>
 		<Header />
 		<main>
 			{children}
 		</main>
-	</Grid>
+	</>
 )
