6a43212b77c02550ba6a6460ac50f4a7c14bd759
Richard Zilahi <richard.zilahi@salesframe.com>
2020-11-23 10:44:35 +0200

    added scss mixins for typefaces

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -10,7 +10,7 @@ export const Header = () => (
 	<header className={
 		classnames(
 			styles.header,
-			"hidden md:flex md:pl-10 md:pr-10 md:space-x-8 justify-center items-center",
+			"md:flex md:pl-10 md:pr-10 md:space-x-8 justify-center items-center",
 		)
 		}>
 		<div className="md:flex w-3/5 justify-between">
@@ -21,7 +21,7 @@ export const Header = () => (
 					</Link>
 				</li>
 			</ul>
-			<ul className="flex space-x-4">
+			<ul className="hidden md:flex space-x-4">
 				<li>
 					<Link href="https://github.com/zilahir">
 						<GitHubIcon htmlColor="#f4abc4" />
