afe721f899f7bfde92b132692bf4f51537c70d64
Richard Zilahi <richard.zilahi@salesframe.com>
2020-11-23 02:57:42 +0200

    added taliwing + postcss config

--- src/components/common/Layout/index.jsx
+++ src/components/common/Layout/index.jsx
@@ -1,11 +1,13 @@
-import { Header } from '@/components/Header';
 import React from 'react';
 
+import { Header } from '@/components/Header';
+import { Grid } from 'react-flexbox-grid';
+
 export const Layout = ({children}) => (
-	<>
+	<Grid fluid>
 		<Header />
 		<main>
 			{children}
 		</main>
-	</>
+	</Grid>
 )
